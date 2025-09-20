b6b45b55e15b5a8436c670180f03c80fed730cd3
RODRIGO-SILVEIRA1 <rodrigosillva069@gmail.com>
2025-09-20 15:25:23 -0300

    Categoria

--- biblioteca/src/main/java/com/example/biblioteca/Services/CategoriaService.java
+++ biblioteca/src/main/java/com/example/biblioteca/Services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.biblioteca.Services;
 
+import com.example.biblioteca.Entities.Categoria;
 import com.example.biblioteca.Repositories.CategoriaRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Getter
@@ -16,4 +19,18 @@ import org.springframework.stereotype.Service;
 public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    //Esse metodo categoria está acessando o repoositório e buscando todas as categorias armazenadas nele
+    public List<Categoria> getCategoria() {
+        return categoriaRepository.findAll();
+    }
+
+    public void createCategoria(String nome){
+
+        Categoria categoria = new Categoria();
+
+        categoria.setName(nome);
+
+        categoriaRepository.save(categoria);
+    }
 }
